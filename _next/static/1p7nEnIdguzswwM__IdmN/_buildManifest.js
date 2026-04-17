896541fd346c4816b6fb6474d9378a9880f264de
Mohammad Ibrahim Hussaini <moh.ibrahimhussaini@gmail.com>
2026-04-17 15:08:11 +0430

    Updates

--- _next/static/1p7nEnIdguzswwM__IdmN/_buildManifest.js
+++ _next/static/1p7nEnIdguzswwM__IdmN/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Portifolio//_next/:path+",
+        "destination": "/Portifolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
