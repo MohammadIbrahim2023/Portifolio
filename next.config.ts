03ab5faad862c6fff3b2bc1af4cf76d3054fc189
Mohammad Ibrahim Hussaini <moh.ibrahimhussaini@gmail.com>
2026-04-17 15:08:42 +0430

    changes in config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Portifolio",
+  assetPrefix: "/Portifolio/",
 };
 
 export default nextConfig;
